93f0270241d1b4d9fc9c65bf58915dd19dd1bc7a
wends05 <cxnner05@gmail.com>
2024-04-27 18:55:26 +0800

    layout done

--- app/home/_layout.jsx
+++ app/home/_layout.jsx
@@ -1,19 +1,27 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-import { Stack } from "expo-router";
+import { Slot } from "expo-router";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 
 export default function Layout() {
   return (
-    <View>
+    <View style={styles.container}>
       <Header />
-      <Stack />
+      <View style={{
+        alignItems:"center",
+      }}>
+      <Slot />
+
+      </View>
       <Footer />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+  },
 });
